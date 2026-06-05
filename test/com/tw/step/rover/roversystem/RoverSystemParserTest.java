c7c1023e879323647185af2c49ac666ed303205d
BhargaviPeddakota <bhargavicme22040@gmail.com>
2026-06-05 10:12:17 +0530

    adds changes to github

--- test/com/tw/step/rover/roversystem/RoverSystemParserTest.java
+++ test/com/tw/step/rover/roversystem/RoverSystemParserTest.java
@@ -1,28 +1,32 @@
 package com.tw.step.rover.roversystem;
 
-import com.tw.step.rover.boundary.InfinitePlateau;
 import com.tw.step.rover.commands.CommandCreator;
 import com.tw.step.rover.position.Navigator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 class RoverSystemParserTest {
     @Test
     void shouldParseAndExecuteRoverSystem() {
-        RoverSystemScanner scanner = RoverSystemScanner.from("""
-            5 5
-            R1 1 2 N
-            R1: RFF
-            """);
-        RoverSystemParser parser = new RoverSystemParser(scanner, Navigator.create(), new CommandCreator());
+//        RoverSystemScanner scanner = RoverSystemScanner.from("""
+//            5 5
+//            R1 1 2 N
+//            R1: RFF
+//            """);
+        RoverSystemScanner scanner = mock(RoverSystemScanner.class);
+        Navigator Navigator  = mock(Navigator.class);
+        CommandCreator commandCreator = mock(CommandCreator.class);
+        RoverSystemParser parser = new RoverSystemParser(scanner,Navigator,commandCreator);
 
         RoverSystem roverSystem = parser.parse();
         roverSystem.execute();
 
         assertEquals("3 2 EACTIVE", roverSystem.toString());
     }
+
 
     @Test
     void shouldExecuteCommandsForMultipleRovers() {
